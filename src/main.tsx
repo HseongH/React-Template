62bdeb78d82c8493c3f0cb0fc422c2c52f7861e2
HseongH <hh4518@gmail.com>
2024-03-12 00:45:51 +0900

    recoil 패키지 추가 및 기본 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from '@/App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </QueryClientProvider>,
 );
