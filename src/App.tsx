8ad8cb9dab4bcfd0e9224f86e5741be6d1c5ace9
HseongH <hh4518@gmail.com>
2024-07-18 01:57:06 +0900

    디자인 시스템 변경 mui -> tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <div />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </RecoilRoot>
     </QueryClientProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import './index.css';
+
 const router = createBrowserRouter([
   {
     path: '/',
