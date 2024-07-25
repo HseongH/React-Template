6275bc3256b5fa49f2dbe2b747229ac7a4283887
HseongH <hh4518@gmail.com>
2024-07-26 01:25:06 +0900

    필요 패키지 변경

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,11 @@
-import { Camera } from 'react-feather';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Card() {
   return (
     <div className="mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-lg">
       <div className="shrink-0">
-        <Camera className="text-5xl text-sky-400" />
+        <FontAwesomeIcon icon={faCamera} className="text-5xl text-sky-400" />
       </div>
       <div>
         <div className="text-xl font-medium text-black">ChitChat</div>
