e32c6251b4d8c844668544df9441a9ee24c7963b
HseongH <hh4518@gmail.com>
2024-07-30 23:56:13 +0900

    headless ui, heroicon 추가

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,10 @@
-import { faCamera } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { CameraIcon } from '@heroicons/react/24/solid';
 
 function Card() {
   return (
     <div className="mx-auto flex max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-lg">
       <div className="shrink-0">
-        <FontAwesomeIcon icon={faCamera} className="text-5xl text-sky-400" />
+        <CameraIcon className="size-7 text-sky-400" />
       </div>
       <div>
         <div className="text-xl font-medium text-black">ChitChat</div>
