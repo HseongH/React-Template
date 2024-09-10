38abf3e7a4defb7ee150f63e9364f026b585617a
HseongH <hh4518@gmail.com>
2024-09-11 01:19:43 +0900

    client 상태 관리 모듈을 recoil에서 zustand로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RecoilRoot } from 'recoil';
 
 import App from '@/App';
 
@@ -11,8 +10,6 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <App />
   </QueryClientProvider>,
 );
